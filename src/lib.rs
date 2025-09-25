3dcce5edc08ae4e102b14ff5e125f39045460577
Tunglies <tunglies.dev@outlook.com>
2025-09-25 15:34:51 +0800

    feat: /run for Linux and macOS

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,12 @@ pub use core::IpcCommand;
 #[cfg(feature = "standalone")]
 pub use core::{run_ipc_server, set_ipc_socket_permissions, stop_ipc_server};
 
-#[cfg(unix)]
+#[cfg(linux)]
 pub static IPC_PATH: &str = "/run/verge/clash-verge-service-ipc.sock";
+
+#[cfg(macos)]
+pub static IPC_PATH: &str = "/private/var/run/verge/clash-verge-service-ipc.sock";
+
 #[cfg(windows)]
 pub static IPC_PATH: &str = r"\\.\pipe\clash-verge-service-ipc";
 
